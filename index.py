e46b5ff55a6f7b23c24573ee3d63a2d972fe02f3
Jose Antonio <bladingnordeste@gmail.com>
2022-06-29 01:17:42 -0300

    Update index.py

--- index.py
+++ index.py
@@ -403,26 +403,60 @@
 
 
 
-Value_FIX_TN = float(15.000)
-Pergunta = 'S'
-TONELADAS = 0
-while Pergunta == 'S':
-    t = int(input('digite o numero de toneladas transportadas: ')) 
-    print(t,'t')
-    Pergunta = str(input('deseja continuar calculando ? [{S}/{N}]  ')).upper()
-if  Pergunta == 'S':
-    TONELADAS += t 
-    print(f'valor total de toneladas: {TONELADAS}t')
-else:
-    res = Value_FIX_TN*t
-    print(f'o valor total faturado foi R${res:,.3f}')
+# Value_FIX_TN = float(15.000)
+# Pergunta = 'S'
+# TONELADAS = 0
+# while Pergunta == 'S':
+#     t = int(input('digite o numero de toneladas transportadas: ')) 
+#     print(t,'t')
+#     Pergunta = str(input('deseja continuar calculando ? [{S}/{N}]  ')).upper()
+# if  Pergunta == 'S':
+#     TONELADAS += t 
+#     print(f'valor total de toneladas: {TONELADAS}t')
+# else:
+#     res = Value_FIX_TN*t
+#     print(f'o valor total faturado foi R${res:,.3f}')
     
+"""EXERCICIO 3 - PORTATORES DE COVID19"""
+
+
+pergunta = 'y'
+for i in pergunta:
+    pl = int(input('digite numero de pacientes com sintomas leves: '))
+    pa = int(input('digite numero de pacientes com sintomas assintomaticos: '))
+    pg = int(input('digite numero de pacientes com sintomas graves: '))
+    pergunta = str(input('aperte "Y" para confirmar: ')).lower()
+    calc = round(pl*100/10) 
+    calc1 = round(pa*100/10)
+    calc2 = (round(pg*100/10))
+    if pergunta != 'y':
+        print('ultilize apenas "Y" como resposta')
+    elif calc > 10*100/10 or calc == calc1 :
+        print('erro')
+    elif calc1 > 10*100/10 or calc1 == calc2 or calc1 == calc:
+        print('erro')
+    elif calc2 > 10*100/10 or calc2 == calc1 or calc2 == calc:
+        print('erro')
+    else:
         
-    
- 
-
-
-
-    
+        print(f'sintomas leves = {calc}%')
+        print(f'sintomas assintomatico {calc1}%')
+        print(f'sintomas graves {calc2}%')
     
 
+# pergunta = 'y'
+# for i in pergunta:
+#     pl = int(input('digite numero de pacientes com sintomas leves: '))
+#     pa = int(input('digite numero de pacientes com sintomas assintomaticos: '))
+#     pg = int(input('digite numero de pacientes com sintomas graves: '))
+#     pergunta = str(input('aperte "Y" para confirmar: ')).lower()
+#     calc = round(pl*100/10) 
+#     calc1 = round(pa*100/10)
+#     calc2 = (round(pg*100/10))
+#     if pergunta != 'y':
+#         print('ultilize apenas "Y" como resposta')
+#     else:
+        
+#         print(f'sintomas leves = {calc}%')
+#         print(f'sintomas assuntomatico = {calc1}%')
+#         print(f'sintomas graves = {calc2}%')
