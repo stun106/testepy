877ac05e27424ea931026670a8c6d0c355291cb4
Jose Antonio <bladingnordeste@gmail.com>
2022-06-30 22:06:21 -0300

    Update index.py

--- index.py
+++ index.py
@@ -420,28 +420,28 @@
 """EXERCICIO 3 - PORTATORES DE COVID19"""
 
 
-pergunta = 'y'
-for i in pergunta:
-    pl = int(input('digite numero de pacientes com sintomas leves: '))
-    pa = int(input('digite numero de pacientes com sintomas assintomaticos: '))
-    pg = int(input('digite numero de pacientes com sintomas graves: '))
-    pergunta = str(input('aperte "Y" para confirmar: ')).lower()
-    calc = round(pl*100/10) 
-    calc1 = round(pa*100/10)
-    calc2 = (round(pg*100/10))
-    if pergunta != 'y':
-        print('ultilize apenas "Y" como resposta')
-    elif calc > 10*100/10 or calc == calc1 :
-        print('erro')
-    elif calc1 > 10*100/10 or calc1 == calc2 or calc1 == calc:
-        print('erro')
-    elif calc2 > 10*100/10 or calc2 == calc1 or calc2 == calc:
-        print('erro')
-    else:
+# pergunta = 'y'
+# for i in pergunta:
+#     pl = int(input('digite numero de pacientes com sintomas leves: '))
+#     pa = int(input('digite numero de pacientes com sintomas assintomaticos: '))
+#     pg = int(input('digite numero de pacientes com sintomas graves: '))
+#     pergunta = str(input('aperte "Y" para confirmar: ')).lower()
+#     calc = round(pl*100/10) 
+#     calc1 = round(pa*100/10)
+#     calc2 = (round(pg*100/10))
+#     if pergunta != 'y':
+#         print('ultilize apenas "Y" como resposta')
+#     elif calc > 10*100/10 or calc == calc1 :
+#         print('erro')
+#     elif calc1 > 10*100/10 or calc1 == calc2 or calc1 == calc:
+#         print('erro')
+#     elif calc2 > 10*100/10 or calc2 == calc1 or calc2 == calc:
+#         print('erro')
+#     else:
         
-        print(f'sintomas leves = {calc}%')
-        print(f'sintomas assintomatico {calc1}%')
-        print(f'sintomas graves {calc2}%')
+#         print(f'sintomas leves = {calc}%')
+#         print(f'sintomas assintomatico {calc1}%')
+#         print(f'sintomas graves {calc2}%')
     
 
 # pergunta = 'y'
@@ -460,3 +460,18 @@ for i in pergunta:
 #         print(f'sintomas leves = {calc}%')
 #         print(f'sintomas assuntomatico = {calc1}%')
 #         print(f'sintomas graves = {calc2}%')
+
+
+"""EXEMPLO 4 - VOGAIS DIGITADAS"""
+# vogal = 0
+# palavra = str(input('digite qualquer palavra: '))
+# for vogais in palavra:
+#     if vogais == 'a' or vogais == 'e' or vogais == 'i' or vogais == 'o'  or vogais == 'u':
+#         print(vogais)
+#         vogal+=1
+# print(vogal, 'vogais')
+
+
+
+# APOSTILHA 3 - REPETIÇÃO E CICLO
+
