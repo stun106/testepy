bf8e60b01e242ed1b4ac39802be0b2fba3a5d4a5
Jose Antonio <bladingnordeste@gmail.com>
2022-07-30 00:51:21 -0300

    att

--- Rascunho.py
+++ Rascunho.py
@@ -224,20 +224,20 @@
 
 # print(primos)
 
-n1 = 50 
+# n1 = 50 
 
-n2 = 5 
+# n2 = 5 
 
-while n1 != n2: 
+# while n1 != n2: 
 
-    if n1<n2:
+#     if n1<n2:
 
-        n2 = n2 - n1
+#         n2 = n2 - n1
 
-    else: 
+#     else: 
 
-        n1 = n1- n2 
+#         n1 = n1- n2 
 
-print(n1)
+# print(n1)
 
         
