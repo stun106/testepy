88f81e04fae58df74f7d780507db58ecf8e87933
Jose Antonio <bladingnordeste@gmail.com>
2022-08-03 01:28:15 -0300

    att

--- Python/função6.py
+++ Python/função6.py
@@ -1,35 +1,50 @@
-from random import randrange
+
+# from random import randrange
+# from time import sleep
+
+# def Start():
+#     n = input('Digite o nome do jogador: ')
+#     p = input('press "Y" para Iniciar: ')
+#     return (n+p)
+    
+     
+# Start()
+
+# def ParDeDado():
+#     a=randrange(2,12)
+#     return a
+
+# while True:
+#     print('-'*30)
+#     sleep(0.5)
+#     print('preparando pra jogar!')
+#     sleep(0.5)
+#     PardeDado = ParDeDado()
+#     if PardeDado == 7 or PardeDado == 11:
+#         print(f'Plick, Plick... Você tirou {PardeDado}')
+#         print('você é um "natural" e venceu o jogo')
+#     elif PardeDado >= 4 and PardeDado < 7 and PardeDado >= 8 and PardeDado <=10:
+#         print('que sorte, vc não perdeu mas pode jogar mais uma vez!')
+#         sleep(0.5)
+#         print(f'Plick, Plick... Você tirou {PardeDado}')
+#     else:
+#         print(f'Plick, Plick... Você tirou {PardeDado}')
+#         sleep(1.0)
+#         print('You Loose, Sir.Craps!')
+        
 
 
-def ParDeDado(b,c):
-    a=randrange(b,c)
-    return a
-
-def ContadorDePontos(pontos,c,a,b):
-    while True:
-        if ParDeDado(a,b) > 4 and ParDeDado(a,b) <=5 or ParDeDado(a,b) == 6 or ParDeDado(a,b)== 8 or ParDeDado(a,b) > 9 and ParDeDado(a,b)<=10:
-            c=0
-            pontos +=c
-            return pontos
-        else:
-            break
 
 
-cont = 0
-valor = 0
-ContadorDePontos(valor,cont,1,12)
 
 
+   
     
 
-# Dado = ParDeDado(1,12)
-# if Dado == 7 or Dado == 11:
-#     print(f'Você tirou: {Dado}. Você é um jogador natural !')
-# elif Dado > 4 and Dado <=5 or Dado == 6 or Dado == 8 or Dado > 9 and Dado <=10:
-  
-    
-# else: 
-#     print(f'Você tirou: {Dado}. Você foi nomeado a Sir.LooserCraps"')
+
+
+
+
 
 
 
